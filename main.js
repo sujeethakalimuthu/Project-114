85a6096823d440568a661a24ea6cb063e0a5a315
sujeethakalimuthu <50768606+sujeethakalimuthu@users.noreply.github.com>
2023-01-15 20:22:33 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload()
+{
+
+}
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+}
+function draw()
+{
+
+}
+function take_snapshot()
+{
+    save("filter.tiff");
+}
